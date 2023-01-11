1ac354c7415ef83dbaac4c163a3d44ec74f3cad4
Nitin Singh <vortechcorp@gmail.com>
2023-01-11 21:04:46 +0530

    created albumsApi using rtk querry to get albums associated with the and also added muataion to create new album

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,8 +1,47 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumList({ user }) {
+    const { data, error, isLoading} = useFetchAlbumsQuery(user);
+    const [addAlbum, result] = useAddAlbumMutation();
+    // console.log(data);
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    };
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} />
+    } else if (error) {
+        content = <div>Error loading data...</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
+
+            return( 
+            <ExpandablePanel key={album.id} header={header}>
+                List of albums
+            </ExpandablePanel>
+            )
+        });
+    }
+
   return (
-    <div>AlbumList {user.name}</div>
-  );
+    <div>
+        <div>
+            Albums for {user.name}
+            <Button onClick={handleAddAlbum} >
+                + Add Album
+            </Button>
+        </div>
+        <div>
+            {content}
+        </div>
+    </div>
+    );
 };
 
 export default AlbumList
--- src/store/index.js
+++ src/store/index.js
@@ -51,21 +51,30 @@
 //  CRUD store
 
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
 
 
 const store = configureStore({
     reducer: {
-        users: usersReducer
+        users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware);       
     }
 });
+
+setupListeners(store.dispatch);
 
 export { store };
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser';
-
+export { useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumsApi';
 
 
 
